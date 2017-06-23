b53d34b56fd43f5492f3426a3a856118afb7e2af
MSDN-SPECIAL\Administrator <minseong9098@gmail.com>
2017-06-23 17:08:24 +0900

    complain service imlp 추가

---
+++ src/main/java/space/complain/model/impl/ComplainService.java
@@ -0,0 +1,16 @@
+package space.complain.model.impl;
+
+import java.util.List;
+
+import space.complain.domain.ComplainVO;
+
+public interface ComplainService {
+	public void complainProduct(ComplainVO complainVO)throws Exception;
+	//상품 신고
+	public ComplainVO getComplain(int cNo)throws Exception;
+	//특정 신고글 조회
+	public List<ComplainVO> getAllComplain()throws Exception;
+	//전체 신고글 조회
+	public List<ComplainVO> findComplainById(String id)throws Exception;
+	//조건부 상품 검색
+}
---
+++ src/main/java/space/complain/model/impl/ComplainServiceImpl.java
@@ -0,0 +1,33 @@
+package space.complain.model.impl;
+
+import java.util.List;
+
+import space.complain.domain.ComplainVO;
+
+public class ComplainServiceImpl implements ComplainService{
+
+	@Override
+	public void complainProduct(ComplainVO complainVO) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ComplainVO getComplain(int cNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ComplainVO> getAllComplain() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ComplainVO> findComplainById(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
